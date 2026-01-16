1a7a9e65a4bf0230347586ff9cd15a1b11ff8f7c
Mariyah Rohim <mariyah.rohim@bankofengland.co.uk>
2026-01-16 13:34:32 +0000

    passes test

--- tests/test_anagram.py
+++ tests/test_anagram.py
@@ -1,9 +1,9 @@
 import unittest
+from anagram import is_anagram
 
 class SmokeTest(unittest.TestCase):
     def test_basic_true(self):
-        self.assertEqual(is_anagram("listen","silent"))
+        self.assertTrue(is_anagram("listen","silent"))
 
 if __name__ == "__main__":
     unittest.main()
-    
